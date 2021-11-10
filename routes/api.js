5aafa2f54f304b2c3eba9ea85929cffb53959934
fabil <87578425+xfar05@users.noreply.github.com>
2021-11-10 13:23:19 +0700

    Add files via upload

--- routes/api.js
+++ routes/api.js
@@ -431,7 +431,6 @@ router.get('/canvas/xnxx', async (req, res) => {
 	     }
     })
 router.get('/telesticker', async(req, res) => {
-  try {
 	      let url = req.query.url
 	      if (!url) return res.json(loghandler.noturl)
 let packName = url.replace("https://t.me/addstickers/", "")
@@ -447,14 +446,11 @@ let packName = url.replace("https://t.me/addstickers/", "")
         let gasIn = await fetch(`https://api.telegram.org/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/getFile?file_id=${fileId}`)
 
         let jisin = await gasIn.json()
+    }
   res.json({
     url: "https://api.telegram.org/file/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/" + jisin.result.file_path
   })
-    }
-	   } catch(err) {
-		    console.log(err)
-		    res.json(loghandler.error)
-	     }
+    
     })
     router.get('/igdl', async(req, res) => {
 	     let url = req.query.url
